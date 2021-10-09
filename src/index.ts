7dec47e116f00279a9ae9af6174347c6fa724a7d
Mortynex <mortynex@outlook.com>
2021-10-09 13:58:32 +0200

    feat(src/index.ts): you can now have multiple languages

--- src/index.ts
+++ src/index.ts
@@ -1,61 +1,94 @@
 import {
-  getInterpolationFunctionKeys,
-  getFunctionParams,
-  getTranslationKeys,
-  t18Options,
-  Translations,
-  UnionOmit,
-  ValidValues,
+	getInterpolationFunctionKeys,
+	getFunctionParams,
+	getTranslationKeys,
+	t18Options,
+	Translations,
+	UnionOmit,
+	ValidValues,
+	TranslationFile,
 } from './typings';
 
-export class t18<T extends Translations> {
-  private translations: Translations;
-  constructor(options: t18Options<T>) {
-    this.translations = options.translations;
-  }
+export class t18<T extends Translations, L extends string> {
+	private _translations: TranslationFile<T, L>[];
+	private _currentLanguage: L;
+	constructor({ translations }: t18Options<T, L>) {
+		if (translations.length === 0) {
+			throw new Error('Invalid translation length');
+		}
 
-  t<Key extends getInterpolationFunctionKeys<T>>(
-    key: Key,
-    args: getFunctionParams<T, Key>
-  ): string;
-  t(
-    key: UnionOmit<getTranslationKeys<T>, getInterpolationFunctionKeys<T>>
-  ): string;
-  t(key: any, args?: any): string {
-    if (typeof key !== 'string') {
-      return 'INVALID KEY';
-    }
+		this._translations = translations;
 
-    const objects = key.split('.');
+		const defaultLanguage =
+			translations.length > 1
+				? translations.find(trans => trans.default === true)?.language
+				: translations[0].language;
 
-    let currentValue: ValidValues | Translations = this.translations;
-    for (const object of objects) {
-      if (typeof currentValue !== 'object') {
-        return 'INVALID PATH';
-      }
+		if (!defaultLanguage) {
+			throw new Error('Default translation file was not set');
+		}
 
-      currentValue = currentValue[object];
-    }
+		this._currentLanguage = defaultLanguage;
+	}
 
-    const translation = currentValue;
+	changeLanguage(lng: L) {
+		this._currentLanguage = lng;
+	}
 
-    if (typeof translation === 'function') {
-      if (!args || !Array.isArray(args)) {
-        return 'INVALID INTERPOLATION FUNCTION ARGUMENTS';
-      }
+	private _getTranslations(lng?: L) {
+		const translations = this._translations.find(
+			file => file.language === (lng ?? this._currentLanguage)
+		)?.translation;
 
-      const withInterpolationTranslation = translation(...args);
-      if (typeof withInterpolationTranslation !== 'string') {
-        return 'INVALID VALUE';
-      }
+		if (translations === undefined) {
+			throw new Error('Invalid language');
+		}
 
-      return withInterpolationTranslation;
-    }
+		return translations;
+	}
 
-    if (typeof translation === 'object') {
-      return 'INVALID PATH';
-    }
+	t<Key extends getInterpolationFunctionKeys<T>>(
+		key: Key,
+		args: getFunctionParams<T, Key>
+	): string;
+	t(
+		key: UnionOmit<getTranslationKeys<T>, getInterpolationFunctionKeys<T>>
+	): string;
+	t(key: any, args?: any): string {
+		if (typeof key !== 'string') {
+			return 'INVALID KEY';
+		}
 
-    return translation;
-  }
+		const objects = key.split('.');
+
+		let currentValue: ValidValues | Translations = this._getTranslations();
+		for (const object of objects) {
+			if (typeof currentValue !== 'object') {
+				return 'INVALID PATH';
+			}
+
+			currentValue = currentValue[object];
+		}
+
+		const translation = currentValue;
+
+		if (typeof translation === 'function') {
+			if (!args || !Array.isArray(args)) {
+				return 'INVALID INTERPOLATION FUNCTION ARGUMENTS';
+			}
+
+			const withInterpolationTranslation = translation(...args);
+			if (typeof withInterpolationTranslation !== 'string') {
+				return 'INVALID VALUE';
+			}
+
+			return withInterpolationTranslation;
+		}
+
+		if (typeof translation === 'object') {
+			return 'INVALID PATH';
+		}
+
+		return translation;
+	}
 }
--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,34 +1,86 @@
 import { t18 } from '../src';
 
 describe('index', () => {
-  describe('t18', () => {
-    it('should translate key to message', () => {
-      const message = 'Hello World!';
+	describe('t18', () => {
+		it('should translate key to message', () => {
+			const message = 'Hello World!';
 
-      const translations = new t18({
-        translations: {
-          general: {
-            hello_word: 'Hello World!',
-          },
-        },
-      });
+			const translations = new t18({
+				translations: [
+					{
+						language: 'en',
+						translation: {
+							general: {
+								hello_world: message,
+							},
+						},
+					},
+				],
+			});
 
-      expect(translations.t('general.hello_word')).toMatch(message);
-    });
+			expect(translations.t('general.hello_world')).toBe(message);
+		});
 
-    it('should return welcome message with name', () => {
-      const message = (name: string) => `Welcome ${name}!`;
-      const name = 'Bob';
+		it('it should be able to change languages ', () => {
+			const messageEn = 'Hello World!';
+			const messageEs = 'Hola Mundo!';
+			const messageIt = 'Ciao Mondo!';
 
-      const translations = new t18({
-        translations: {
-          messages: {
-            welcome: message,
-          },
-        },
-      });
+			const translations = new t18({
+				translations: [
+					{
+						language: 'en',
+						translation: {
+							general: {
+								hello_world: messageEn,
+							},
+						},
+						default: true,
+					},
+					{
+						language: 'es',
+						translation: {
+							general: {
+								hello_world: messageEs,
+							},
+						},
+					},
+					{
+						language: 'it',
+						translation: {
+							general: {
+								hello_world: messageIt,
+							},
+						},
+					},
+				],
+			});
 
-      expect(translations.t('messages.welcome', [name])).toMatch(message(name));
-    });
-  });
+			expect(translations.t('general.hello_world')).toBe(messageEn);
+			translations.changeLanguage('es');
+			expect(translations.t('general.hello_world')).toBe(messageEs);
+			translations.changeLanguage('it');
+			expect(translations.t('general.hello_world')).toBe(messageIt);
+		});
+
+		it('should return welcome message with name', () => {
+			const message = (name: string) => `Welcome ${name}!`;
+			const name = 'Bob';
+
+			const translations = new t18({
+				translations: [
+					{
+						language: 'en',
+						translation: {
+							general: {
+								welcome: message,
+							},
+						},
+					},
+				],
+			});
+
+			expect(translations.t('general.welcome', [name])).toMatch(message(name));
+		});
+	});
 });
